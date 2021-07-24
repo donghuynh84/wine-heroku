27e2844a71cfec34d29feb80194b799cb61e2f37
donghuynh84 <71362661+donghuynh84@users.noreply.github.com>
2021-07-24 11:50:34 +1000

    Update wine_prediction.py

--- wine_prediction.py
+++ wine_prediction.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import Pickle
 from sklearn import datasets
 from sklearn.datasets import load_wine
 from sklearn.metrics import accuracy_score
